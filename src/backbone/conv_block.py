34bcd13032e19dfd6617fa6b74365e4a11237176
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-10 17:58:51 +0300

    Create conv_block.py

---
+++ src/backbone/conv_block.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.leaky_relu = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.leaky_relu(x)
+        return x
